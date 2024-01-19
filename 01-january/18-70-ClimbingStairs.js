7fad7da2e215bc23af0496b51fa44aedbd85a8e0
ValentinTT <valentintapiatorti@gmail.com>
2024-01-19 09:54:37 -0300

    fix January - day 18

--- 01-january/18-70-ClimbingStairs.js
+++ 01-january/18-70-ClimbingStairs.js
@@ -4,3 +4,20 @@
  * @version 0.1
  * @date 18-01-2023
  */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  if(n === 1) return 1;
+  if(n === 2) return 2;
+  
+  let steps = new Array(n);
+  steps[0] = 1;
+  steps[1] = 2;
+  
+  for(let i = 2; i < n; i++) {
+      steps[i] = steps[i-1] + steps[i-2];
+  }
+  return steps[n-1];
+};
